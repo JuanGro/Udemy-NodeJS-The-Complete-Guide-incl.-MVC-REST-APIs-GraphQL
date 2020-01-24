8f6164c331bf6520d9bba021a9c43a00d4290125
JuanMa <ing.juangro@gmail.com>
2020-01-24 17:51:52 -0600

    Add chapter 34

--- Section 3: Understanding the Basics/34. Parsing Request Bodies.js
+++ Section 3: Understanding the Basics/34. Parsing Request Bodies.js
@@ -7,16 +7,27 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></input></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
         res.write('</html>');
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY SAMPLE');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
     }
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
